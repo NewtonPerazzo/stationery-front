bad11f7356264a8cb4d655c406346d86e13fb77f
Newton Perazzo <perazzoneto99@hotmail.com>
2026-09-03 10:06:29 -0300

    App integration added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
+import App from './App'
+import { theme } from './app/theme'
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 )
